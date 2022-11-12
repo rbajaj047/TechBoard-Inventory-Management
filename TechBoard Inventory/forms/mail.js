2127be05d5f61c55f20d195ede9baaaf2b021761
Sai Srinivas <94926227+Sai-004@users.noreply.github.com>
2022-11-12 07:43:11 +0530

    Complete CSS and Added Options page HTML

--- TechBoard Inventory/forms/mail.js
+++ TechBoard Inventory/forms/mail.js
@@ -6,14 +6,14 @@ const firebaseConfig = {
     storageBucket: "inventoryform-a52f2.appspot.com",
     messagingSenderId: "403613700903",
     appId: "1:403613700903:web:7b944b024b9231b5ae8f02"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 var inventoryFormDB = firebase.database().ref("inventoryForm");
-document.getElementById("inventoryForm").addEventListener("submit",submitForm);
+document.getElementById("inventoryForm").addEventListener("submit", submitForm);
 
-function submitForm(e){
+function submitForm(e) {
 
     e.preventDefault();
     var a = getElementVal("a");
@@ -23,26 +23,26 @@ function submitForm(e){
     var e = getElementVal("e");
     var f = getElementVal("f");
     var g = getElementVal("g");
-    
+
     var h = getElementVal("h");
 
-    console.log(a,b,c,d,e,f,g,h);
+    console.log(a, b, c, d, e, f, g, h);
 
-    saveMessages(a,b,c,d,e,f,g,h);
+    saveMessages(a, b, c, d, e, f, g, h);
 
 
-    
+
 }
-const saveMessages = (a, b, c, d, e, f, g, h)=> {
+const saveMessages = (a, b, c, d, e, f, g, h) => {
     var newInventoryForm = inventoryFormDB.push({
-        a : a,
-        b : b,
-        c : c,
-        d : d,
-        e : e,
-        f : f,
-        g : g,
-        h : h,
+        a: a,
+        b: b,
+        c: c,
+        d: d,
+        e: e,
+        f: f,
+        g: g,
+        h: h,
 
     });
     const postid = newInventoryForm.key;
@@ -52,7 +52,7 @@ const getElementVal = (id) => {
     console.log(id);
     try {
         return document.getElementById(id).value;
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         return "";
     }
